24d5d48e68ebae842f1dc1ca715475265064f9a9
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-04-12 17:06:07 -0500

    update memory

--- myapp/blog_posts/views.py
+++ myapp/blog_posts/views.py
@@ -23,3 +23,26 @@ def create_post():
 def blog_post(blog_post_id):
   blog_post = BlogPost.query.get_or_404(blog_post_id)
   return render_template('blog_post.html', title=blog_post.title, date=blog_post.date, post=blog_post)
+
+@blog_posts.route('/<int:blog_post_id>/update',methods=['GET','POST'])
+@login_required
+def update(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+
+    if blog_post.author != current_user:
+        abort(403)
+
+    form = BlogPostForm()
+
+    if form.validate_on_submit():
+        blog_post.title = form.title.data
+        blog_post.text = form.text.data
+        db.session.commit()
+        flash('Memory Updated')
+        return redirect(url_for('blog_posts.blog_post',blog_post_id=blog_post.id))
+
+    elif request.method == 'GET':
+        form.title.data = blog_post.title
+        form.text.data = blog_post.text
+
+    return render_template('create_post.html',title='Updating',form=form)
