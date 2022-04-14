b0286f17e4f8ac036cebb215b28fdb75bf9afc32
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-04-14 17:58:18 -0500

    revise core route views

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -3,11 +3,15 @@ from myapp.models import BlogPost
 
 core = Blueprint('core', __name__)
 
-@core.route('/')
+@core.route('/index')
 def index():
+    return render_template('index.html')
+
+@core.route('/posts')
+def posts():
     page = request.args.get('page', 1, type=int)
     blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
-    return render_template('index.html', blog_posts=blog_posts)
+    return render_template('memory_lane.html', blog_posts=blog_posts)
 
 @core.route('/info')
 def info():
